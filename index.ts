550254ecdd705898aca3516daff4fd63719e27d2
VeejaLiu <veejaliu@gmail.com>
2023-09-20 09:35:07 +0800

    init code

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
-import express from "express";
+import express from 'express';
 
 const router = express.Router();
 
 router.get('/health_check', (req, res) => {
   res.send('ok');
-})
+});
 
 router.post('/health_check', (req, res) => {
   res.send({
     message: 'ok',
-    data: {...req.body}
+    data: { ...req.body },
   });
-})
+});
 
 export default router;
