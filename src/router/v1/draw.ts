0bac7c818ee6c4ed85d133268dc761472d096135
VeejaLiu <veejaliu@gmail.com>
2023-09-23 02:18:12 +0800

    add draw api

--- src/router/v1/draw.ts
+++ src/router/v1/draw.ts
@@ -1,26 +1,32 @@
 import express from 'express';
-import { draw } from '../../service/draw';
-import path from 'path';
-import fs from 'fs';
 
+const fetch = require('node-fetch') 
 const router = express.Router();
 
+import { Logger } from '../../lib/logger';
+const logger = new Logger(__filename);
+
 router.post('', async (req, res) => {
-    console.log(`[API_LOGS][/draw] ${JSON.stringify(req.body)}`);
+
+    logger.info(`[API_LOGS][/draw] ${JSON.stringify(req.body)}`);
     const { prompt } = req.body;
-    const result = await draw(prompt);
 
-    const filePath = path.join(__dirname, `../../public/${result.fileName}`);
-    const stream = fs.createReadStream(filePath);
-
-    res.set('Content-Type', 'image/jpeg');
-    stream.on('data', (chunk) => {
-        res.write(chunk);
+    const response = await fetch('http://127.0.0.1:10102/draw', {
+        method: 'POST',
+        // request a image
+        headers: {
+            'Content-Type': 'application/json',
+        },  
+        body: JSON.stringify({
+            prompt: `${prompt}`,
+        }),
     });
-    stream.on('end', () => {
-        res.status(200);
-        res.end();
-    });
+    const buffer = await response.buffer();
+    const base64 = buffer.toString('base64');
+    
+    // result is an image in base64 format
+    // res.set('Content-Type', 'image/png');
+    res.send(buffer);
 });
 
 export default router;
