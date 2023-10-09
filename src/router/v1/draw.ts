b640d402c109048334d9fc785aacc91d10759104
VeejaLiu <veejaliu@gmail.com>
2023-10-09 11:58:53 +0800

    修改绘制流程

--- src/router/v1/draw.ts
+++ src/router/v1/draw.ts
@@ -2,25 +2,26 @@ import express from 'express';
 import { draw } from '../../service/draw';
 import path from 'path';
 import fs from 'fs';
+import { putObject } from '../../clients/minio/minio';
+import { Logger } from '../../lib/logger';
 
 const router = express.Router();
 
+const log = new Logger(__filename);
+
 router.post('', async (req, res) => {
-    console.log(`[API_LOGS][/draw] ${JSON.stringify(req.body)}`);
+    log.info(`[API_LOGS][/draw] ${JSON.stringify(req.body)}`);
     const { prompt } = req.body;
     const result = await draw(prompt);
 
     const filePath = path.join(__dirname, `../../public/${result.fileName}`);
-    const stream = fs.createReadStream(filePath);
 
-    res.set('Content-Type', 'image/jpeg');
-    stream.on('data', (chunk) => {
-        res.write(chunk);
-    });
-    stream.on('end', () => {
-        res.status(200);
-        res.end();
-    });
+    // const stream = fs.createReadStream(filePath);
+    const buffer = fs.readFileSync(filePath);
+    await putObject(result.fileName, buffer);
+
+    res.status(200);
+    res.end();
 });
 
 export default router;
