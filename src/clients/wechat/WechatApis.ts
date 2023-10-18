7a3260dfb4f3be3840a9dde7ce52b2a4e5fd9371
VeejaLiu <veejaliu@gmail.com>
2023-10-18 17:09:23 +0800

    Add WechatApis.ts

---
+++ src/clients/wechat/WechatApis.ts
@@ -0,0 +1,43 @@
+import { getOsEnv } from '../../lib/env';
+import { Logger } from '../../lib/logger';
+
+const logger = new Logger('WechatApis');
+
+export interface WechatCode2SessionResponse {
+    openid: string;
+    session_key: string;
+}
+
+export class WechatApis {
+    private static appId: string = getOsEnv('APP_ID');
+    private static appSecret = getOsEnv('APP_SECRET');
+
+    static async code2session(code: string): Promise<WechatCode2SessionResponse> {
+        try {
+            // 调用https://api.weixin.qq.com/sns/jscode2session接口获取openid
+            const wxRes = await fetch(
+                `https://api.weixin.qq.com/sns/jscode2session?` +
+                    `appid=${this.appId}` +
+                    `&` +
+                    `secret=${this.appSecret}` +
+                    `&` +
+                    `js_code=${code}` +
+                    `&` +
+                    `grant_type=authorization_code`,
+                {
+                    method: 'GET',
+                },
+            );
+
+            const wxResJson = await wxRes.json();
+            if (wxResJson.errcode) {
+                throw new Error(wxResJson.errcode);
+            }
+
+            return wxResJson;
+        } catch (e) {
+            logger.error(`[code2session] ${e}`);
+            throw e;
+        }
+    }
+}
