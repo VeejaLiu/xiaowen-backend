8934c02205ced6a0ffd7a188e65b1fe4a2316fc1
VeejaLiu <veejaliu@gmail.com>
2023-12-03 18:36:19 +0800

    注释admin入口

--- src/router/v1/index.ts
+++ src/router/v1/index.ts
@@ -6,7 +6,7 @@ router.use('/draw', require('./draw').default);
 router.use('/user', require('./user').default);
 router.use('/quota', require('./quota').default);
 router.use('/login', require('./login').default);
-router.use('/admin', require('./admin').default);
+// router.use('/admin', require('./admin').default);
 router.use('/history', require('./history').default);
 router.use('/notification', require('./notification').default);
 export default router;
