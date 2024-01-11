23e58bf62d4add44effa48d3ae80e7c06fb37ee7
VeejaLiu <veejaliu@gmail.com>
2024-01-12 01:48:26 +0800

    增加生成逻辑

--- src/clients/generate-server/generate.ts
+++ src/clients/generate-server/generate.ts
@@ -1,18 +1,7 @@
-const fetch = require('node-fetch');
+import { GenerateConfig, TATTOO_STYLE } from './type';
+import { getGenerateConfig } from './generateConfig';
 
-export enum TATTOO_STYLE {
-    BLACK_WORK = 0,
-    DOT_WORK = 1,
-    GEOMETRIC = 2,
-    WATERCOLOR = 3,
-    REALISM = 4,
-    NEO_TRADITIONAL = 5,
-    NEW_SCHOOL = 6,
-    JAPANESE = 7,
-    TRIBAL = 8,
-    LETTERING = 9,
-    TRASH_POLKA = 10,
-}
+const fetch = require('node-fetch');
 
 /**
  * Get images path from generate server
@@ -20,31 +9,19 @@ export enum TATTOO_STYLE {
  * @param body
  * @returns images path of minio, `bucketName/objectName`
  */
-export async function draw(body: {
-    style: TATTOO_STYLE;
-    prompt: string;
-}): Promise<{ images: string[]; used_time: number }> {
-    const response = await fetch('http://127.0.0.1:10102/draw', {
-        method: 'POST',
-        // request a image
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            prompt: `${body.prompt}`,
-            style: body.style,
-        }),
-    });
-    return await response.json();
+export async function draw({ style, prompt }: { style: TATTOO_STYLE; prompt: string }): Promise<{ images: string[] }> {
+    // Get generate config
+    const generateConfig: GenerateConfig = await getGenerateConfig({ style, prompt });
 
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-    // return {
-    //     images: [
-    //         `tattoo/${new Date().valueOf()}_1.jpg`,
-    //         `tattoo/${new Date().valueOf()}_2.jpg`,
-    //         `tattoo/${new Date().valueOf()}_3.jpg`,
-    //         `tattoo/${new Date().valueOf()}_4.jpg`,
-    //     ],
-    //     used_time: 3000,
-    // };
+    // Send request to generate server
+
+    // Get images base64 from generate server
+    const images = [];
+
+    // generate thumbnail
+
+    // upload original image and thumbnail to minio
+
+    // return images path of minio
+    return { images };
 }
---
+++ src/clients/generate-server/type.ts
@@ -0,0 +1,19 @@
+export enum TATTOO_STYLE {
+    DOT_WORK = 1, // 点刺
+    BLACK_WORK = 2, // 纯黑
+    MINIMALIST = 3, // 小清新
+    GEOMETRIC = 4, // 几何
+    OLD_SCHOOL = 5, // Old School
+    NEW_SCHOOL = 6, // New School
+    JAPANESE = 7, // 日式
+    REALISM = 8, // 现实
+    TRASH_POLKA = 9, // 垃圾波尔卡
+    TRIBAL = 10, // 部落
+}
+
+export interface GenerateConfig {
+    prompt: string;
+    negativePrompt: string;
+    height: number;
+    width: number;
+}
